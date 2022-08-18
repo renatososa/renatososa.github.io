32e7d599108ad2cebeab2b9abbc4066a1a062ba3
renatososa <renato.sosast@gmail.com>
2022-08-18 16:20:35 -0300

    modificación del login
    Funcionalidad del Oauth de google

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-function login_1(){
+function login(){
         if(document.getElementById("login").text === "Salir"){
         localStorage.setItem("email", null);
         window.location = "index.html";
@@ -25,18 +25,17 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 const email = localStorage.getItem("email");
-const PSW = localStorage.getItem("PSW");
-const login_btn = document.getElementById("login");
+const nombre = localStorage.getItem("name");
 const login_menu = document.getElementById("login_menu");
-if(email!=null && PSW!=null && email!="null"){
+if(email!=null && email!="null"){
     const menu = 
-    `<a class="nav-link">Renato</a>
+    `<a class="nav-link">` + nombre + `</a>
     <ul class="navbar-nav w-100 justify-content-between">
         <li class="nav-item">
             <a class="nav-link" href="my-profile.html">Mi perfil</a>
         </li>
         <li class="nav-item">
-        <a class="nav-link" id="login" onclick="login_1()">Salir</a>
+        <a class="nav-link" id="login" onclick="login()">Salir</a>
         </li>
     </ul>
     `
@@ -45,7 +44,7 @@ if(email!=null && PSW!=null && email!="null"){
 else{
     const menu = 
     `
-    <a class="nav-link" id="login" onclick="login_1()">Login</a>
+    <a class="nav-link" id="login" onclick="login()">Login</a>
     `
     login_menu.innerHTML = menu;
 }
--- js/login.js
+++ js/login.js
@@ -2,5 +2,11 @@ function login(){
     var email = document.getElementById("floatingInput").value;
     var password = document.getElementById("floatingPassword").value;
     localStorage.setItem("email", email);
-    localStorage.setItem("PSW", password);
+    localStorage.setItem("name", email.slice(0, 6));
 }
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    localStorage.setItem("email", profile.getEmail());
+    localStorage.setItem("name", profile.getName());
+  }
