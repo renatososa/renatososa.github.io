4ee2de8609995033733cdc1c22522056ff17f55e
renatososa <renato.sosast@gmail.com>
2022-08-18 16:38:48 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -9,5 +9,5 @@ function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     localStorage.setItem("email", profile.getEmail());
     localStorage.setItem("name", profile.getName());
-    document.location = "index.html";
+    document.location.href = "index.html";
   }
