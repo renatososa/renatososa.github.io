12f43b33ec71fd9d2b34f91f2584dc6a6b578bc2
renatososa <renato.sosast@gmail.com>
2022-08-18 16:05:58 -0300

    Menú desplegable Login
    Añadido de Menú desplegable en Login, boton de regresar en págin del login

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function login_1(){
-        if(document.getElementById("login").text === "Logout"){
+        if(document.getElementById("login").text === "Salir"){
         localStorage.setItem("email", null);
         window.location = "index.html";
         }
@@ -27,9 +27,25 @@ document.addEventListener("DOMContentLoaded", function(){
 const email = localStorage.getItem("email");
 const PSW = localStorage.getItem("PSW");
 const login_btn = document.getElementById("login");
+const login_menu = document.getElementById("login_menu");
 if(email!=null && PSW!=null && email!="null"){
-    login_btn.innerHTML = "Logout";
+    const menu = 
+    `<a class="nav-link">Renato</a>
+    <ul class="navbar-nav w-100 justify-content-between">
+        <li class="nav-item">
+            <a class="nav-link" href="my-profile.html">Mi perfil</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" id="login" onclick="login_1()">Salir</a>
+        </li>
+    </ul>
+    `
+    login_menu.innerHTML = menu;
 }
 else{
-    login_btn.innerHTML = "Login";
+    const menu = 
+    `
+    <a class="nav-link" id="login" onclick="login_1()">Login</a>
+    `
+    login_menu.innerHTML = menu;
 }
